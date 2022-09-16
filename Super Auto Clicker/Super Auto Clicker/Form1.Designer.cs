2df6775a9345415dbc606652c6cddd00fd4071b5
Egloff Nicola BBB <89132258+Nicola3341246@users.noreply.github.com>
2022-09-16 09:30:26 +0200

    hallo
    V1 vo eusem projekt

--- Super Auto Clicker/Super Auto Clicker/Form1.Designer.cs
+++ Super Auto Clicker/Super Auto Clicker/Form1.Designer.cs
@@ -29,11 +29,64 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(182, 153);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(187, 27);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Interval";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Super Auto Clicker/Super Auto Clicker/Form1.cs
+++ Super Auto Clicker/Super Auto Clicker/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Super_Auto_Clicker
 {
     public partial class Form1 : Form
@@ -5,6 +7,40 @@ namespace Super_Auto_Clicker
         public Form1()
         {
             InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwdata, int dwextrainfo);
+        public enum mouseeventflags
+        {
+            LeftDown = 2,
+            LeftUp = 4,
+        }
+
+        public void leftclick(Point p)
+        {
+            mouse_event((int)(mouseeventflags.LeftDown), p.X, p.Y, 0, 0);
+            mouse_event((int)(mouseeventflags.LeftUp), p.X, p.Y, 0, 0);
+        }
+        bool stop = true;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stop = (stop) ? false : true;
+            timer1.Interval = (int)numericUpDown1.Value;
+            timer1.Enabled = true;
+            if (!stop)
+            {
+                timer1.Start();
+            }
+            if (stop)
+            {
+                timer1.Stop();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            leftclick(new Point(MousePosition.X, MousePosition.Y));
         }
     }
 }
