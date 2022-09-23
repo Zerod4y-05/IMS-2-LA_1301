aff0d3dedbba9db40466e181a06362538a817e3e
TOP997 <89131744+TOP997@users.noreply.github.com>
2022-09-23 07:48:15 +0200

    Geil

--- Super Auto Clicker/Super Auto Clicker/Form1.Designer.cs
+++ Super Auto Clicker/Super Auto Clicker/Form1.Designer.cs
@@ -86,7 +86,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(346, 122);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button1);
